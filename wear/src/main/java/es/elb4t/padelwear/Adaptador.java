3caab59d5a40b8f735c6f393701b4a66129fe33f
Eloy Llacer <eloy4t@gmail.com>
2017-05-31 21:38:35 +0200

    Crear una lista de selección

---
+++ wear/src/main/java/es/elb4t/padelwear/Adaptador.java
@@ -0,0 +1,64 @@
+package es.elb4t.padelwear;
+
+import android.content.Context;
+import android.support.wearable.view.WearableRecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by eloy on 31/5/17.
+ */
+
+final class Adaptador extends WearableRecyclerView.Adapter {
+    private String[] datos;
+    private final Context contexto;
+    private final LayoutInflater inflador;
+    protected View.OnClickListener onClickListener;
+
+    public Adaptador(Context contexto, String[] datos) {
+        this.contexto = contexto;
+        this.datos = datos;
+        inflador = LayoutInflater.from(contexto);
+    }
+
+    //Creamos nuestro ViewHolder, con los tipos de elementos a modificar
+    public static class ItemViewHolder extends WearableRecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.nombre);
+        }
+    }
+
+    // Creamos el ViewHolder con las vista de un elemento sin personalizar
+    @Override
+    public WearableRecyclerView.ViewHolder onCreateViewHolder(
+            // Inflamos la vista desde el xml
+            ViewGroup parent, int viewType) {
+        View v = inflador.inflate(R.layout.elemento_lista, null);
+        v.setOnClickListener(onClickListener);
+        return new ItemViewHolder(v);
+    }
+
+    // Usando como base el ViewHolder y lo personalizamos
+    @Override
+    public void onBindViewHolder(WearableRecyclerView.ViewHolder holder, int posicion) {
+        ItemViewHolder itemHolder = (ItemViewHolder) holder;
+        TextView view = itemHolder.textView;
+        view.setText(datos[posicion]);
+        holder.itemView.setTag(posicion);
+    }
+
+    // Indicamos el número de elementos de la lista
+    @Override
+    public int getItemCount() {
+        return datos.length;
+    }
+
+    public void setOnItemClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+}
