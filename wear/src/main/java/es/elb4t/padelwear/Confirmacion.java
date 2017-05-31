e4802188ef01cb1a8de49346798e894c677a3d6c
Eloy Llacer <eloy4t@gmail.com>
2017-05-31 23:49:33 +0200

    Asociar una acción al pulsar un elemento de la lista

---
+++ wear/src/main/java/es/elb4t/padelwear/Confirmacion.java
@@ -0,0 +1,16 @@
+package es.elb4t.padelwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by eloy on 31/5/17.
+ */
+
+public class Confirmacion extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmacion);
+    }
+}
--- wear/src/main/java/es/elb4t/padelwear/MainActivity.java
+++ wear/src/main/java/es/elb4t/padelwear/MainActivity.java
@@ -1,6 +1,7 @@
 package es.elb4t.padelwear;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.support.wearable.view.WearableRecyclerView;
@@ -25,7 +26,11 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Integer tag = (Integer) v.getTag();
-                Toast.makeText(MainActivity.this, "Elegida opción:" + tag, Toast.LENGTH_SHORT).show();
+                switch (tag) {
+                    case 1:
+                        startActivity(new Intent(MainActivity.this, Confirmacion.class));
+                        break;
+                }
             }
         });
 
